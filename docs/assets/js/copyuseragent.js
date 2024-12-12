176a79454f0469931a04d56e1a83debd28498da1
hafizulwanandaputra <wanadaputrahafizul@gmail.com>
2024-12-12 12:53:22 +0700

    Add docs

---
+++ docs/assets/js/copyuseragent.js
@@ -0,0 +1,6 @@
+function copyUserAgent() {
+  var copyText = document.getElementById("useragent");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+}
---
+++ docs/assets/js/time.js
@@ -0,0 +1,39 @@
+function startTime() {
+	var today = new Date();
+	var months = [
+		"Jan",
+		"Feb",
+		"Mar",
+		"Apr",
+		"Mei",
+		"Jun",
+		"Jul",
+		"Agu",
+		"Sep",
+		"Okt",
+		"Nov",
+		"Des",
+	];
+	var days = ["Min", "Sen", "Sel", "Rab", "Kam", "Jum", "Sab"];
+	var y = today.getFullYear();
+	var month = months[today.getMonth()];
+	var d = today.getDate();
+	var day = days[today.getDay()];
+	var h = today.getHours();
+	var m = today.getMinutes();
+	var s = today.getSeconds();
+	d = checkTime(d);
+	month = checkTime(month);
+	h = checkTime(h);
+	m = checkTime(m);
+	s = checkTime(s);
+	document.getElementById("tanggalwaktu").innerHTML =
+		day + " " + d + " " + month + " " + y + " - " + h + "." + m + "." + s;
+	var t = setTimeout(startTime, 1);
+}
+function checkTime(i) {
+	if (i < 10) {
+		i = "0" + i;
+	} // add zero in front of numbers < 10
+	return i;
+}
