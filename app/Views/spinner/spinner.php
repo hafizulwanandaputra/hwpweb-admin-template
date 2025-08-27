a54a163f9f6814bc1f5e2fc79b04f416dba6eaa2
hafizulwanandaputra <wanadaputrahafizul@gmail.com>
2025-08-27 08:41:11 +0700

    Font Awesome v7.0.0

--- app/Views/spinner/spinner.php
+++ app/Views/spinner/spinner.php
@@ -1,4 +1,4 @@
-<span class="loadingio-spinner-spinner-nq4q5u6dq7r" style="font-size: 1em" class="px-2">
+<span class="loadingio-spinner-spinner-nq4q5u6dq7r" style="font-size: 1em; margin-right: 0.125em; margin-left: 0.125em;">
     <div class="ldio-x2uulkbinbj">
         <div></div>
         <div></div>
